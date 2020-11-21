7a2ed0c5ef5600044a41b867cb85054ad0306229
Kai Luo <gluokai@gmail.com>
2020-11-21 13:16:02 +0800

    Use old Translator

--- core/translator.go
+++ core/translator.go
@@ -5,36 +5,22 @@ package core
 import (
 	"bufio"
 	"bytes"
+	"encoding/binary"
+	"errors"
 	"io"
 	"io/ioutil"
-	"net"
 	"net/http"
-	"time"
 )
 
 type Translator interface {
-	Translate(in, out net.Conn, stop chan struct{}) error
+	Translate(in io.Reader, out io.Writer) error
 }
 
 type Repeater struct{}
 
-const DEFAULT_COPY_SIZE = 4096
-const DEFAULT_TIMEOUT = 120
-
-func (this *Repeater) Translate(in, out net.Conn, stop chan struct{}) error {
-	for {
-		select {
-		case <-stop:
-			return nil
-		default:
-			in.SetReadDeadline(time.Now().Add(DEFAULT_TIMEOUT * time.Second))
-			out.SetReadDeadline(time.Now().Add(2 * DEFAULT_TIMEOUT * time.Second))
-			_, err := io.CopyN(out, in, DEFAULT_COPY_SIZE)
-			if err != nil {
-				return err
-			}
-		}
-	}
+func (this *Repeater) Translate(in io.Reader, out io.Writer) error {
+	_, err := io.Copy(out, in)
+	return err
 }
 
 type HTTPPacker struct {
@@ -43,33 +29,26 @@ type HTTPPacker struct {
 
 const HTTP_BUFFER_SIZE = 1 << 16
 
-func (this *HTTPPacker) Translate(in, out net.Conn, stop chan struct{}) error {
+func (this *HTTPPacker) Translate(in io.Reader, out io.Writer) error {
 	buf := make([]byte, HTTP_BUFFER_SIZE)
 	for {
-		select {
-		case <-stop:
-			return nil
-		default:
-			in.SetReadDeadline(time.Now().Add(DEFAULT_TIMEOUT * time.Second))
-			n, err := in.Read(buf)
-			if err != nil {
-				return err
-			}
-			body, err := this.P.RunOnBytes(buf[:n])
-			if err != nil {
-				return err
-			}
-			req, err := http.NewRequest("POST", "/", bytes.NewReader(body))
-			if err != nil {
-				return err
-			}
-			req.Host = DEFAULT_HOST
-			req.Header.Add("User-Agent", TLS_APP_PROTO)
-			out.SetWriteDeadline(time.Now().Add(DEFAULT_TIMEOUT * time.Second))
-			err = req.Write(out)
-			if err != nil {
-				return err
-			}
+		n, err := in.Read(buf)
+		if err != nil {
+			return err
+		}
+		body, err := this.P.RunOnBytes(buf[:n])
+		if err != nil {
+			return err
+		}
+		req, err := http.NewRequest("POST", "/", bytes.NewReader(body))
+		if err != nil {
+			return err
+		}
+		req.Host = DEFAULT_HOST
+		req.Header.Add("User-Agent", TLS_APP_PROTO)
+		err = req.Write(out)
+		if err != nil {
+			return err
 		}
 	}
 }
@@ -78,33 +57,77 @@ type HTTPUnpacker struct {
 	P Pass
 }
 
-func (this *HTTPUnpacker) Translate(in, out net.Conn, stop chan struct{}) error {
+func (this *HTTPUnpacker) Translate(in io.Reader, out io.Writer) error {
 	b := bufio.NewReader(in)
 	for {
-		select {
-		case <-stop:
+		req, err := http.ReadRequest(b)
+		if err != nil {
+			return err
+		}
+		req.Host = DEFAULT_HOST
+		req.Header.Add("User-Agent", TLS_APP_PROTO)
+		defer req.Body.Close()
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			return err
+		}
+		if body, err = this.P.RunOnBytes(body); err != nil {
+			return err
+		}
+		_, err = out.Write(body)
+		if err != nil {
+			return err
+		}
+	}
+}
+
+type LVPacker struct{}
+
+type LVUnpacker struct{}
+
+func (this *LVPacker) Translate(in io.Reader, out io.Writer) error {
+	l := make([]byte, binary.MaxVarintLen64)
+	b := make([]byte, HTTP_BUFFER_SIZE)
+	for {
+		n, err := in.Read(b)
+		if err != nil {
+			if err != io.EOF && err != io.ErrUnexpectedEOF {
+				return err
+			}
 			return nil
-		default:
-			in.SetReadDeadline(time.Now().Add(DEFAULT_TIMEOUT * time.Second))
-			req, err := http.ReadRequest(b)
-			if err != nil {
+		}
+		binary.PutUvarint(l, uint64(n))
+		if _, err = out.Write(l); err != nil {
+			return err
+		}
+		if _, err = out.Write(b[:n]); err != nil {
+			return err
+		}
+	}
+}
+
+func (this *LVUnpacker) Translate(in io.Reader, out io.Writer) error {
+	l := make([]byte, binary.MaxVarintLen64)
+	for {
+		if _, err := io.ReadFull(in, l); err != nil {
+			if err != io.EOF && err != io.ErrUnexpectedEOF {
 				return err
 			}
-			req.Host = DEFAULT_HOST
-			req.Header.Add("User-Agent", TLS_APP_PROTO)
-			defer req.Body.Close()
-			body, err := ioutil.ReadAll(req.Body)
-			if err != nil {
+			return nil
+		}
+		x, n := binary.Uvarint(l)
+		if n <= 0 {
+			return errors.New("Can't read a valid length")
+		}
+		b := make([]byte, x)
+		if _, err := io.ReadFull(in, b); err != nil {
+			if err != io.EOF && err != io.ErrUnexpectedEOF {
 				return err
 			}
-			if body, err = this.P.RunOnBytes(body); err != nil {
-				return err
-			}
-			out.SetWriteDeadline(time.Now().Add(DEFAULT_TIMEOUT * time.Second))
-			_, err = out.Write(body)
-			if err != nil {
-				return err
-			}
+			return nil
+		}
+		if _, err := out.Write(b); err != nil {
+			return err
 		}
 	}
 }
