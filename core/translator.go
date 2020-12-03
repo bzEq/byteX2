661cfb54d82c77acdb27790308ceafe2d76433ad
Kai Luo <gluokai@gmail.com>
2020-12-03 12:36:36 +0800

    Revert "Set buffer size to 16k"
    This reverts commit 78b15dd2893f96a6f6b46587287319ed3c652706.

--- core/translator.go
+++ core/translator.go
@@ -31,7 +31,7 @@ type HTTPPacker struct {
 	P Pass
 }
 
-const HTTP_BUFFER_SIZE = 16 << 10
+const HTTP_BUFFER_SIZE = 2 << 20
 
 func (this *HTTPPacker) Translate(in net.Conn, out net.Conn) error {
 	buf := make([]byte, HTTP_BUFFER_SIZE)
