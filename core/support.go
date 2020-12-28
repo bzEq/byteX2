f9aa566d82e68cce78fce0730ca13bdd96bddc25
Kai Luo <gluokai@gmail.com>
2020-12-29 00:07:57 +0800

    Remove tls config

--- core/support.go
+++ core/support.go
@@ -3,16 +3,13 @@
 package core
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
 	"io"
-	"math/big"
 	"net"
 	"sync"
 )
+
+const TLS_APP_PROTO = "com.wandu.spider"
+const DEFAULT_HOST = "www.wandu.com"
 
 type OnceCloser struct {
 	once sync.Once
@@ -30,32 +27,5 @@ func (this *OnceCloser) Close() error {
 
 func MakePipe() (pipe [2]net.Conn) {
 	pipe[0], pipe[1] = net.Pipe()
-	return
-}
-
-const TLS_APP_PROTO = "com.wandu.spider"
-const DEFAULT_HOST = "www.wandu.com"
-
-func CreateBarebonesTLSConfig() (c *tls.Config, err error) {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		return
-	}
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		return
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		return
-	}
-	c = &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-		NextProtos:   []string{TLS_APP_PROTO},
-	}
 	return
 }
